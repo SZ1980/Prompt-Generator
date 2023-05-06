09cdc86dfa41fdbbed86d4907402b52ce3c22879
Zoran Simonovic <simonovic1980@gmail.com>
2023-05-06 14:11:14 +0200

    starting server

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,15 +11,17 @@ function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-        <main className="app">
-          <Nav />
-          <Provider />
-          {children}
-        </main>
+          <main className="app">
+            <Nav />
+
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
